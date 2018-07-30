4ce8435695b1d786b8a87386b274ecdc77e7be5a
Anatyoliyyy <anatoliyyym@ukr.net>
2018-07-30 21:17:16 +0300

    Initial commit

---
+++ src/Lesson3Homework2/com/gmail/agemtup/TextConteiner.java
@@ -0,0 +1,14 @@
+package Lesson3Homework2.com.gmail.agemtup;
+
+import java.lang.annotation.*;
+
+@Retention(RetentionPolicy.RUNTIME)
+@interface SaveTo {
+    String path();
+}
+
+@SaveTo(path="saver.txt")
+public class TextConteiner {
+    String text = "Hello World";
+}
+
